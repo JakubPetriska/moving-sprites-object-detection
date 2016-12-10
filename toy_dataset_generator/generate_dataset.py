b15ae95f767cf50c9c92b1ca50b41ab782ba5c87
Jakub Petriska <jakubpetriska@gmail.com>
2016-12-10 21:36:06 +0800

    all important constants moved to constants file, validation, training and testing data are now being generated

--- toy_dataset_generator/generate_dataset.py
+++ toy_dataset_generator/generate_dataset.py
@@ -12,20 +12,6 @@ SHOW_VIDEO_ENCODING_INFO_LOG = False
 SHOW_TIME_LOG = True
 SHOW_OBJECT_RECTANGLES = True
 
-DATASET_IMAGES_DIR = 'images'
-DATASET_VIDEO_FILE = 'video.mp4'
-DATASET_LABELS_FILE = 'labels.txt'
-
-FRAME_IMAGE_FILE_NAME_FORMAT = 'image%05d.png'
-
-AVERAGE_SPRITE_COUNT = 2
-SPRITE_MIN_SCALE = 0.1
-
-ALLOW_SHEARING = False
-
-MEAN_SPRITE_MOVEMENT_SPEED = constants.DATASET_RESOLUTION_WIDTH / 7  # Pixels per second
-MEAN_SPRITE_SCALE_SPEED = 0.01  # Absolute scale per second
-
 
 def apply_gaussian_noise(frame):
     noise = np.random.normal(size=frame.shape, scale=10)
@@ -59,21 +45,21 @@ class Sprite:
         bottom = top + scaled_sprite_image_size[0]
         left = position[1]
         right = left + scaled_sprite_image_size[1]
-        if bottom < 0 or top >= constants.DATASET_RESOLUTION_HEIGHT \
-                or right < 0 or left >= constants.DATASET_RESOLUTION_WIDTH:
+        if bottom < 0 or top >= constants.RESOLUTION_HEIGHT \
+                or right < 0 or left >= constants.RESOLUTION_WIDTH:
             return False
         else:
             scaled_sprite = misc.imresize(self.sprite_image, scaled_sprite_image_size)
             # Take only the visible part of sprite
             overlap_top = max(top * -1, 0)
-            overlap_bottom = max(bottom - constants.DATASET_RESOLUTION_HEIGHT, 0)
+            overlap_bottom = max(bottom - constants.RESOLUTION_HEIGHT, 0)
             overlap_left = max(left * -1, 0)
-            overlap_right = max(right - constants.DATASET_RESOLUTION_WIDTH, 0)
+            overlap_right = max(right - constants.RESOLUTION_WIDTH, 0)
             scaled_sprite \
                 = scaled_sprite[overlap_top:scaled_sprite_image_size[0] - overlap_bottom,
                   overlap_left:scaled_sprite_image_size[1] - overlap_right, :]
             position = np.clip(position,
-                               0, max(constants.DATASET_RESOLUTION_HEIGHT, constants.DATASET_RESOLUTION_WIDTH))
+                               0, max(constants.RESOLUTION_HEIGHT, constants.RESOLUTION_WIDTH))
             sprite_alpha = scaled_sprite[:, :, 3] / 255
             background_alpha = -sprite_alpha + 1
             for i in range(0, 3):
@@ -109,24 +95,27 @@ class SequenceGenerator:
         # Linear scale with possible shearing
         self.scale_functions.append(
             lambda initial_scale, lifetime, scale_speed: np.clip(initial_scale + scale_speed * lifetime,
-                                                                 SPRITE_MIN_SCALE, 1))
+                                                                 constants.SPRITE_MIN_SCALE, 1))
 
     def _spawn_sprite(self):
         sprite_image = random.choice(self.sprite_images)
         half_shape = np.array(sprite_image.shape) / 2
-        initial_position = np.array((random.randrange(0, constants.DATASET_RESOLUTION_HEIGHT) - half_shape[0],
-                                     random.randrange(0, constants.DATASET_RESOLUTION_WIDTH) - half_shape[1]))
+        initial_position = np.array((random.randrange(0, constants.RESOLUTION_HEIGHT) - half_shape[0],
+                                     random.randrange(0, constants.RESOLUTION_WIDTH) - half_shape[1]))
         velocity = np.array((random.random() - 0.5, random.random() - 0.5))
         velocity /= np.linalg.norm(velocity)
-        velocity *= random.gauss(mu=MEAN_SPRITE_MOVEMENT_SPEED, sigma=MEAN_SPRITE_MOVEMENT_SPEED / 2)
+        velocity *= random.gauss(mu=constants.MEAN_SPRITE_MOVEMENT_SPEED,
+                                 sigma=constants.MEAN_SPRITE_MOVEMENT_SPEED / 2)
 
-        if ALLOW_SHEARING:
-            initial_scale = np.array([random.uniform(SPRITE_MIN_SCALE, 1) for i in range(0, 2)])
+        if constants.ALLOW_SHEARING:
+            initial_scale = np.array([random.uniform(constants.SPRITE_MIN_SCALE, 1) for i in range(0, 2)])
             scale_speed = np.array(
-                [random.gauss(mu=MEAN_SPRITE_SCALE_SPEED, sigma=MEAN_SPRITE_SCALE_SPEED / 2) for i in range(0, 2)])
+                [random.gauss(mu=constants.MEAN_SPRITE_SCALE_SPEED, sigma=constants.MEAN_SPRITE_SCALE_SPEED / 2)
+                 for i in range(0, 2)])
         else:
-            initial_scale = np.array([random.uniform(SPRITE_MIN_SCALE, 1)] * 2)
-            scale_speed = np.array([random.gauss(mu=MEAN_SPRITE_SCALE_SPEED, sigma=MEAN_SPRITE_SCALE_SPEED / 2)] * 2)
+            initial_scale = np.array([random.uniform(constants.SPRITE_MIN_SCALE, 1)] * 2)
+            scale_speed = np.array([random.gauss(mu=constants.MEAN_SPRITE_SCALE_SPEED,
+                                                 sigma=constants.MEAN_SPRITE_SCALE_SPEED / 2)] * 2)
         if random.random() >= 0.5:
             scale_speed *= -1
         return Sprite(sprite_image, initial_position, initial_scale,
@@ -134,7 +123,7 @@ class SequenceGenerator:
                       scale_speed, random.choice(self.scale_functions))
 
     def _spawn_probability(self):
-        slope = (0.3 - 1) / AVERAGE_SPRITE_COUNT
+        slope = (0.3 - 1) / constants.AVERAGE_SPRITE_COUNT
         return slope * len(self.sprites) + 1
 
     def generate_next_frame(self):
@@ -144,7 +133,7 @@ class SequenceGenerator:
         if random.random() <= self._spawn_probability():
             self.sprites.append(self._spawn_sprite())
 
-        frame = np.ones((constants.DATASET_RESOLUTION_HEIGHT, constants.DATASET_RESOLUTION_WIDTH, 3))
+        frame = np.ones((constants.RESOLUTION_HEIGHT, constants.RESOLUTION_WIDTH, 3))
         frame *= 210
         apply_gaussian_noise(frame)
 
@@ -157,14 +146,17 @@ class SequenceGenerator:
 
 
 def generate_sequence(frame_count, folder_path):
-    images_dir = os.path.join(folder_path, DATASET_IMAGES_DIR)
+    if frame_count <= 0:
+        return
+
+    images_dir = os.path.join(folder_path, constants.DATASET_IMAGES_DIR)
     if not os.path.exists(images_dir):
         os.makedirs(images_dir)
 
     sprite_images = []
     for sprite_file_name in listdir(constants.SPRITES_DIR):
         image = misc.imread(os.path.join(constants.SPRITES_DIR, sprite_file_name))
-        scale_factor = constants.DATASET_RESOLUTION_WIDTH / image.shape[1]
+        scale_factor = constants.RESOLUTION_WIDTH / image.shape[1]
         scaled_shape = [int(d * scale_factor) for d in image.shape]
         scaled_shape[2:] = image.shape[2:]
         image = misc.imresize(image, scaled_shape)
@@ -173,15 +165,15 @@ def generate_sequence(frame_count, folder_path):
     # Generate the video frames
     sequence_generator = SequenceGenerator(sprite_images)
     frame_generation_start = utils.start_timer()
-    frame_image_path_format = os.path.join(images_dir, FRAME_IMAGE_FILE_NAME_FORMAT)
+    frame_image_path_format = os.path.join(images_dir, constants.FRAME_IMAGE_FILE_NAME_FORMAT)
     for i in range(0, frame_count):
         frame = sequence_generator.generate_next_frame()
         misc.imsave(frame_image_path_format % i, frame)
 
     frames_generation_duration = utils.get_duration_secs(frame_generation_start)
-    print('Frames generated')
+    print('\tFrames generated')
     if SHOW_TIME_LOG:
-        print('\tGenerated %d frames in %.1f seconds, average time per frame is %f seconds'
+        print('\t\tGenerated %d frames in %.1f seconds, average time per frame is %f seconds'
               % (frame_count, frames_generation_duration, frames_generation_duration / frame_count))
 
     # Generate video file
@@ -189,11 +181,19 @@ def generate_sequence(frame_count, folder_path):
     os.system('ffmpeg -f image2 -r %d -i %s -loglevel %s -vcodec mpeg4 -y %s'
               % (constants.FRAMES_PER_SECOND, frame_image_path_format,
                  '32' if SHOW_VIDEO_ENCODING_INFO_LOG else '24',
-                 os.path.join(folder_path, DATASET_VIDEO_FILE)))
+                 os.path.join(folder_path, constants.DATASET_VIDEO_FILE)))
 
-    print('Video file generated')
+    print('\tVideo file generated')
     if SHOW_TIME_LOG:
-        print('\tGenerated video in %.1f seconds' % utils.get_duration_secs(video_encoding_start))
+        print('\t\tGenerated video in %.1f seconds' % utils.get_duration_secs(video_encoding_start))
 
 
-generate_sequence(constants.FRAME_COUNT, constants.DATASET_TOP_DIR)
+print('Generating training data')
+generate_sequence(constants.FRAME_COUNT_TRAINING,
+                  os.path.join(constants.OUTPUT_PATH, constants.TRAINING_DATASET_PATH))
+print('Generating validation data')
+generate_sequence(constants.FRAME_COUNT_VALIDATION,
+                  os.path.join(constants.OUTPUT_PATH, constants.VALIDATION_DATASET_PATH))
+print('Generating test data')
+generate_sequence(constants.FRAME_COUNT_TEST,
+                  os.path.join(constants.OUTPUT_PATH, constants.TEST_DATASET_PATH))
