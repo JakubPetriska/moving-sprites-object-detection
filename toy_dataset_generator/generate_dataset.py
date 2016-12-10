cae5e43c2fb1cbcfc783d4102138ae1e5c5605f4
Jakub Petriska <jakubpetriska@gmail.com>
2016-12-10 16:26:11 +0800

    initial commit

---
+++ toy_dataset_generator/generate_dataset.py
@@ -0,0 +1,180 @@
+import os.path
+import random
+from os import listdir
+
+import numpy as np
+from scipy import misc
+
+from toy_dataset_generator import constants
+from utils import utils
+
+SHOW_VIDEO_ENCODING_INFO_LOG = False
+SHOW_TIME_LOG = True
+
+DATASET_IMAGES_DIR = 'images'
+DATASET_VIDEO_FILE = 'video.mp4'
+DATASET_LABELS_FILE = 'labels.txt'
+
+FRAME_IMAGE_FILE_NAME_FORMAT = 'image%05d.png'
+
+AVERAGE_SPRITE_COUNT = 3
+SPRITE_MIN_SCALE = 0.1
+
+ALLOW_SHEARING = False
+
+MEAN_SPRITE_MOVEMENT_SPEED = constants.DATASET_RESOLUTION_WIDTH / 5  # Pixels per second
+MEAN_SPRITE_SCALE_SPEED = 0.3  # Absolute scale per second
+
+
+def apply_gaussian_noise(frame):
+    noise = np.random.normal(size=frame.shape, scale=10)
+    frame += noise
+
+
+class Sprite:
+    def __init__(self, sprite_image, initial_position, initial_scale, velocity, movement_function,
+                 scale_speed, scale_function):
+        self.sprite_image = sprite_image
+        self.lifetime = 0
+        self.initial_position = initial_position
+        self.initial_scale = initial_scale
+        self.velocity = velocity
+        self.movement_function = movement_function
+        self.scale_speed = scale_speed
+        self.scale_function = scale_function
+
+    def increase_lifetime(self):
+        self.lifetime += 1 / constants.FRAMES_PER_SECOND
+
+    def render(self, frame):
+        position = self.movement_function(self.initial_position, self.lifetime, self.velocity)
+        scale = self.scale_function(self.initial_scale, self.lifetime, self.scale_speed)
+
+        sprite_image_size = (round(scale[0] * self.sprite_image.shape[0]), round(scale[1] * self.sprite_image.shape[1]))
+        sprite_image_size = [int(a) for a in sprite_image_size]
+        top = position[0]
+        bottom = position[0] + sprite_image_size[0]
+        left = position[1]
+        right = position[1] + sprite_image_size[1]
+        if (bottom < 0 or top >= constants.DATASET_RESOLUTION_HEIGHT) \
+                and (right < 0 or left >= constants.DATASET_RESOLUTION_WIDTH):
+            return False
+        else:
+            scaled_sprite = misc.imresize(self.sprite_image, sprite_image_size)
+            # Take only the visible part of sprite
+            # TODO
+            # scaled_sprite \
+            #     = scaled_sprite[top * -1:sprite_image_size[0] - (bottom - constants.DATASET_RESOLUTION_HEIGHT),
+            #       left * -1:sprite_image_size[1] - (right - constants.DATASET_RESOLUTION_WIDTH), :]
+            for i in range(0, sprite_image_size[0]):
+                for j in range(0, sprite_image_size[1]):
+                    frame_position = int(position[0] + i), int(position[1] + j)
+                    if 0 <= frame_position[0] < constants.DATASET_RESOLUTION_HEIGHT \
+                            and 0 <= frame_position[1] < constants.DATASET_RESOLUTION_WIDTH:
+                        alpha = scaled_sprite[i, j, 3] / 255.0
+                        background_alpha = 1 - alpha
+                        for k in range(0, 3):
+                            frame[frame_position[0], frame_position[1], k] *= background_alpha
+                            frame[frame_position[0], frame_position[1], k] += alpha * scaled_sprite[i, j, k]
+            return True
+
+
+class SequenceGenerator:
+    def __init__(self, sprite_images):
+        self.sprite_images = sprite_images
+        self.sprites = []
+        self.movement_functions = []
+        self.scale_functions = []
+
+        # Linear movement
+        self.movement_functions.append(
+            lambda initial_position, lifetime, velocity: initial_position + velocity * lifetime)
+        # Linear scale with possible shearing
+        self.scale_functions.append(
+            lambda initial_scale, lifetime, scale_speed: np.clip(initial_scale + scale_speed * lifetime,
+                                                                 SPRITE_MIN_SCALE, 1))
+
+    def _spawn_sprite(self):
+        initial_position = np.array((random.randrange(0, constants.DATASET_RESOLUTION_HEIGHT),
+                                     random.randrange(0, constants.DATASET_RESOLUTION_WIDTH)))
+        velocity = np.array((random.random() - 0.5, random.random() - 0.5))
+        velocity /= np.linalg.norm(velocity)
+        velocity *= random.gauss(mu=MEAN_SPRITE_MOVEMENT_SPEED, sigma=MEAN_SPRITE_MOVEMENT_SPEED / 2)
+
+        if ALLOW_SHEARING:
+            initial_scale = np.array([random.uniform(SPRITE_MIN_SCALE, 1) for i in range(0, 2)])
+            scale_speed = np.array(
+                [random.gauss(mu=MEAN_SPRITE_SCALE_SPEED, sigma=MEAN_SPRITE_SCALE_SPEED / 2) for i in range(0, 2)])
+        else:
+            initial_scale = np.array([random.uniform(SPRITE_MIN_SCALE, 1)] * 2)
+            scale_speed = np.array([random.gauss(mu=MEAN_SPRITE_SCALE_SPEED, sigma=MEAN_SPRITE_SCALE_SPEED / 2)] * 2)
+        if random.random() >= 0.5:
+            scale_speed *= -1
+        return Sprite(random.choice(self.sprite_images), initial_position, initial_scale,
+                      velocity, random.choice(self.movement_functions),
+                      scale_speed, random.choice(self.scale_functions))
+
+    def _spawn_probability(self):
+        slope = (0.3 - 1) / AVERAGE_SPRITE_COUNT
+        return slope * len(self.sprites) + 1
+
+    def generate_next_frame(self):
+        for sprite in self.sprites:
+            sprite.increase_lifetime()
+
+        if random.random() <= self._spawn_probability():
+            self.sprites.append(self._spawn_sprite())
+
+        frame = np.ones((constants.DATASET_RESOLUTION_HEIGHT, constants.DATASET_RESOLUTION_WIDTH, 3))
+        frame *= 210
+        apply_gaussian_noise(frame)
+
+        for i in range(len(self.sprites) - 1, -1, -1):
+            if not self.sprites[i].render(frame):
+                self.sprites.pop(i)
+
+        frame = np.clip(frame, 0, 255).astype(np.uint8)
+        return frame
+
+
+def generate_sequence(frame_count, folder_path):
+    images_dir = os.path.join(folder_path, DATASET_IMAGES_DIR)
+    if not os.path.exists(images_dir):
+        os.makedirs(images_dir)
+
+    sprite_images = []
+    for sprite_file_name in listdir(constants.SPRITES_DIR):
+        image = misc.imread(os.path.join(constants.SPRITES_DIR, sprite_file_name))
+        scale_factor = constants.DATASET_RESOLUTION_WIDTH / image.shape[1]
+        scaled_shape = [int(d * scale_factor) for d in image.shape]
+        scaled_shape[2:] = image.shape[2:]
+        image = misc.imresize(image, scaled_shape)
+        sprite_images.append(image)
+
+    # Generate the video frames
+    sequence_generator = SequenceGenerator(sprite_images)
+    frame_generation_start = utils.start_timer()
+    frame_image_path_format = os.path.join(images_dir, FRAME_IMAGE_FILE_NAME_FORMAT)
+    for i in range(0, frame_count):
+        frame = sequence_generator.generate_next_frame()
+        misc.imsave(frame_image_path_format % i, frame)
+
+    frames_generation_duration = utils.get_duration_secs(frame_generation_start)
+    print('Frames generated')
+    if SHOW_TIME_LOG:
+        print('\tGenerated %d frames in %.1f seconds, average time per frame is %f seconds'
+              % (frame_count, frames_generation_duration, frames_generation_duration / frame_count))
+
+    # Generate video file
+    video_encoding_start = utils.start_timer()
+    os.system('ffmpeg -f image2 -r %d -i %s -loglevel %s -vcodec mpeg4 -y %s'
+              % (constants.FRAMES_PER_SECOND, frame_image_path_format,
+                 '32' if SHOW_VIDEO_ENCODING_INFO_LOG else '24',
+                 os.path.join(folder_path, DATASET_VIDEO_FILE)))
+
+    print('Video file generated')
+    if SHOW_TIME_LOG:
+        print('\tGenerated video in %.1f seconds' % utils.get_duration_secs(video_encoding_start))
+
+
+generate_sequence(constants.FRAME_COUNT, constants.DATASET_TOP_DIR)
---
+++ toy_dataset_generator/sprite_cropper.py
@@ -0,0 +1,64 @@
+# Crop all sprites of any overhanging background.
+
+from os import listdir
+import os.path
+from scipy import misc
+import numpy as np
+
+from toy_dataset_generator import constants
+
+
+file_names = listdir(constants.SPRITES_DIR)
+index = 0
+for file_name in file_names:
+    file_path = os.path.join(constants.SPRITES_DIR, file_name)
+    sprite = misc.imread(file_path)
+    transparent_pixel = np.zeros(sprite.shape[2])
+
+    # Cut out top empty pixels
+    for i in range(0, sprite.shape[0]):
+        should_break = False
+        for j in range(0, sprite.shape[1]):
+            if not np.array_equal(sprite[i, j], transparent_pixel):
+                sprite = sprite[i:, :]
+                should_break = True
+                break
+        if should_break:
+            break
+
+    # Cut out empty bottom pixels
+    for i in range(sprite.shape[0] - 1, -1, -1):
+        should_break = False
+        for j in range(0, sprite.shape[1]):
+            if not np.array_equal(sprite[i, j], transparent_pixel):
+                sprite = sprite[0:i, :]
+                should_break = True
+                break
+        if should_break:
+            break
+
+    # Cut out empty left pixels
+    for j in range(0, sprite.shape[1]):
+        should_break = False
+        for i in range(0, sprite.shape[0]):
+            if not np.array_equal(sprite[i, j], transparent_pixel):
+                sprite = sprite[:, j:]
+                should_break = True
+                break
+        if should_break:
+            break
+
+    # Cut out empty right pixels
+    for j in range(sprite.shape[1] - 1, -1, -1):
+        should_break = False
+        for i in range(0, sprite.shape[0]):
+            if not np.array_equal(sprite[i, j], transparent_pixel):
+                sprite = sprite[:, 0:j]
+                should_break = True
+                break
+        if should_break:
+            break
+
+    misc.imsave(file_path, sprite)
+    index += 1
+    print('%s/%s' % (index, len(file_names)))
